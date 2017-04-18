24ca2090328467e0d111e961560a4bec750262d5
Ganesh Sapkal <ganesh_sapkal@bmc.com>
2017-04-18 12:12:10 -0400

    First cut

---
+++ KidsMath/AdditionController.swift
@@ -0,0 +1,28 @@
+//
+//  AdditionController.swift
+//  KidsMath
+//
+//  Created by Ganesh Sapkal on 4/18/17.
+//  Copyright © 2017 Panaromic Innovations Inc. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class AdditionViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+   
+    
+}
+
---
+++ KidsMath/SubstractionController.swift
@@ -0,0 +1,14 @@
+//
+//  SubstractionController.swift
+//  KidsMath
+//
+//  Created by Ganesh Sapkal on 4/18/17.
+//  Copyright © 2017 Panaromic Innovations Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SubstractionCoontroller :UIViewController {
+    
+}
