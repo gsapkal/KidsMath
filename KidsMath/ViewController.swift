21f19774536323ac9b03843249ea98a10ed629ee
Ganesh Sapkal <ganesh_sapkal@bmc.com>
2017-04-18 11:02:18 -0400

    Initial Commit

---
+++ KidsMath/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KidsMath
+//
+//  Created by Ganesh Sapkal on 4/18/17.
+//  Copyright © 2017 Panaromic Innovations Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
